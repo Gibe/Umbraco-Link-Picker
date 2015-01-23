aa42a89c63422c4ae148004633b624cbffefe40d
Karl Tynan <hello@karltynan.co.uk>
2015-01-23 17:03:11 +0000

    Directory begins and start files

---
+++ src/App_Plugins/GibeLinkPicker/picker.controller.js
@@ -0,0 +1,30 @@
+﻿angular.module("umbraco").controller("Gibe.LinkPickerController", function ($scope, assetsService) {
+
+	var ngi = angular.element('body').injector();
+	var uDialogService = ngi.get('dialogService');
+
+	// choose internal link
+	$scope.chooseLink = function () {
+		$scope.model.value = null;
+		uDialogService.linkPicker({
+			callback: function (e) {
+				// set model
+				$scope.model.value = {
+					id: e.id || 0,
+					name: e.name || '',
+					url: e.url,
+					target: e.target || '_self'
+				};
+				// close dialog
+				uDialogService.closeAll();
+			}
+		});
+	};
+
+	// remove link
+	$scope.removeLink = function () {
+		$scope.model.value = null;
+	};
+
+	assetsService.loadCss("/App_Plugins/GibeLinkPicker/picker.css");
+});
