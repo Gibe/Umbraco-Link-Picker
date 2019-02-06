14fad6bce40ca965fa14afe1b1a02be52de5b2da
Steve Temple <steve@gibedigital.com>
2019-02-06 14:38:25 +0000

    Update styling of link picker to 7.6+

--- src/Gibe.LinkPicker.Umbraco/App_Plugins/GibeLinkPicker/picker.controller.js
+++ src/Gibe.LinkPicker.Umbraco/App_Plugins/GibeLinkPicker/picker.controller.js
@@ -1,56 +1,58 @@
 ﻿angular.module("umbraco").controller("Gibe.LinkPickerController", function ($scope, assetsService) {
 
-	var ngi = angular.element('body').injector();
-	var uDialogService = ngi.get('dialogService');
+    var ngi = angular.element('body').injector();
+    var uDialogService = ngi.get('dialogService');
 
     // choose internal link
-	$scope.chooseLink = function () {
-	    $scope.model.value = null;
-	    uDialogService.open({
-	        template: '../App_Plugins/GibeLinkPicker/Dialogs/linkpicker.html',
-	        show: true,
-	        dialogData: $scope.model.config,
-	        callback: function(e) {
-	            // set model
-	            $scope.model.value = {
-	                id: e.id || 0,
-	                name: e.name || '',
-	                url: e.url,
-	                target: e.target || '_self',
-	                hashtarget: e.hashtarget || ''
-	            };
-	            // close dialog
-	            uDialogService.close();
-	        }
-	    });
-	};
+    $scope.chooseLink = function () {
+        $scope.model.value = null;
+        uDialogService.open({
+            template: '../App_Plugins/GibeLinkPicker/Dialogs/linkpicker.html',
+            show: true,
+            dialogData: $scope.model.config,
+            callback: function (e) {
+                // set model
+                $scope.model.value = {
+                    id: e.id || 0,
+                    name: e.name || '',
+                    icon: e.icon,
+                    url: e.url,
+                    target: e.target || '_self',
+                    hashtarget: e.hashtarget || ''
+                };
+                // close dialog
+                uDialogService.close();
+            }
+        });
+    };
 
-	$scope.editLink = function () {
-	    var linkPickerModel = angular.copy($scope.model);
-	    uDialogService.open({
-	        template: '../App_Plugins/GibeLinkPicker/Dialogs/linkpicker.html',
-	        show: true,
-	        dialogData: linkPickerModel.config,
-	        target: linkPickerModel.value,
-	        callback: function (e) {
-	            // set model
-	            $scope.model.value = {
-	                id: e.id || 0,
-	                name: e.name || '',
-	                url: e.url,
-	                target: e.target || '_self',
-	                hashtarget: e.hashtarget || ''
-	            };
-	            // close dialog
-	            uDialogService.close();
-	        }
-	    });
-	};
+    $scope.editLink = function () {
+        var linkPickerModel = angular.copy($scope.model);
+        uDialogService.open({
+            template: '../App_Plugins/GibeLinkPicker/Dialogs/linkpicker.html',
+            show: true,
+            dialogData: linkPickerModel.config,
+            target: linkPickerModel.value,
+            callback: function (e) {
+                // set model
+                $scope.model.value = {
+                    id: e.id || 0,
+                    name: e.name || '',
+                    icon: e.icon,
+                    url: e.url,
+                    target: e.target || '_self',
+                    hashtarget: e.hashtarget || ''
+                };
+                // close dialog
+                uDialogService.close();
+            }
+        });
+    };
 
-	// remove link
-	$scope.removeLink = function () {
-		$scope.model.value = null;
-	};
+    // remove link
+    $scope.removeLink = function () {
+        $scope.model.value = null;
+    };
 
-	assetsService.loadCss("../App_Plugins/GibeLinkPicker/picker.css");
+    assetsService.loadCss("../App_Plugins/GibeLinkPicker/picker.css");
 });
